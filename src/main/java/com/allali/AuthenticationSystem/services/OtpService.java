f4da31dcd5475e8055c01e8ad8d5f60b616a489b
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2024-02-27 08:41:50 +0100

    Continue with sendOtp

--- src/main/java/com/allali/AuthenticationSystem/services/OtpService.java
+++ src/main/java/com/allali/AuthenticationSystem/services/OtpService.java
@@ -1,12 +1,17 @@
 package com.allali.AuthenticationSystem.services;
 
+import com.allali.AuthenticationSystem.dtos.EmailDetails;
 import com.allali.AuthenticationSystem.dtos.OtpRequest;
 import com.allali.AuthenticationSystem.dtos.OtpResponse;
 import com.allali.AuthenticationSystem.dtos.Response;
+import com.allali.AuthenticationSystem.entities.Otp;
 import com.allali.AuthenticationSystem.repositories.OtpRepository;
+import com.allali.AuthenticationSystem.utils.AppUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
@@ -15,7 +20,23 @@ public class OtpService {
     private final OtpRepository otpRepository ;
     private final EmailService emailService;
 
-    private Response sendOtp(OtpRequest otpRequest){
-        
+    public Response sendOtp(OtpRequest otpRequest){
+        String otp = AppUtils.generateOtp();
+        otpRepository.save(Otp.builder()
+                        .otp(otp)
+                        .email(otpRequest.getEmail())
+                        .expiredAt(LocalDateTime.now().plusMinutes(2))
+                .build());
+        emailService.sendEmail(EmailDetails.builder()
+                        .subject("Do Not Desclose !!")
+                        .destinatiare(otpRequest.getEmail())
+                        .messageBody("This organization has sent you an Otp expires in 2 min")
+                .build());
+
+        return Response.builder()
+                .statusCode(200)
+                .responseMessage("OK")
+                .build();
     }
+
 }
