e12561cbc5013a92fb9c0938883927ff9af0b1d7
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2024-02-27 08:49:04 +0100

    working on controller

--- src/main/java/com/allali/AuthenticationSystem/controllers/OtpController.java
+++ src/main/java/com/allali/AuthenticationSystem/controllers/OtpController.java
@@ -1,2 +1,14 @@
-package com.allali.AuthenticationSystem.controllers;public class OtpController {
+package com.allali.AuthenticationSystem.controllers;
+
+import com.allali.AuthenticationSystem.services.OtpService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/otp")
+@AllArgsConstructor
+public class OtpController {
+    private final OtpService otpService ;
+
 }
